1c30a2b1f0a972cf531b62ff2d5ea46e944355e7
Diego <200066@iessanalberto.com>
2024-09-25 09:03:03 +0200

    Modificacion size AccountScreen

--- app/src/main/java/com/example/chefhub/screens/AccountScreen.kt
+++ app/src/main/java/com/example/chefhub/screens/AccountScreen.kt
@@ -1,12 +1,35 @@
 package com.example.chefhub.screens
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.AccountCircle
+import androidx.compose.material3.Button
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
+import com.example.chefhub.R
 import com.example.chefhub.scaffold.MyMainBottomBar
 import com.example.chefhub.ui.AppViewModel
 
@@ -31,5 +54,72 @@ fun AccountScreen(navController: NavController, appViewModel: AppViewModel) {
 
 @Composable
 fun AccountScreenContent(navController: NavController, appViewModel: AppViewModel) {
-    // TODO: Diseño de la página.
+    // COMENTARIO.
+    val appUiState by appViewModel.appUiState.collectAsState()
+    val context = LocalContext.current
+
+    // COMENTARIO.
+    Row(
+        horizontalArrangement = Arrangement.Center,
+        verticalAlignment = Alignment.CenterVertically,
+        modifier = Modifier.fillMaxWidth().size(220.dp, 220.dp).padding(20.dp)
+    ) {
+        // COMENTARIO. TODO: POR EL AMOR DE DIOS, ARREGLA EL DISEÑO.
+        Column(
+            verticalArrangement = Arrangement.Top,
+            horizontalAlignment = Alignment.CenterHorizontally,
+        ) {
+            // COMENTARIO.
+            Row(
+                horizontalArrangement = Arrangement.Start,
+                verticalAlignment = Alignment.Top
+            ) {
+                Image(
+                    painter = painterResource(id = R.drawable.icono_usuario_estandar),
+                    contentDescription = "Usuario",
+                    contentScale = ContentScale.Crop,
+                    modifier = Modifier
+                        .size(150.dp)
+                        .clip(CircleShape)
+                )
+            }
+
+            // COMENTARIO.
+            Row(
+                horizontalArrangement = Arrangement.Start,
+                verticalAlignment = Alignment.Top
+            ) {
+                Text("[placeholder]")
+            }
+        }
+
+        // COMENTARIO.
+        Column(
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Center
+        ) {
+            // COMENTARIO.
+            Row(
+                horizontalArrangement = Arrangement.Center,
+                verticalAlignment = Alignment.CenterVertically,
+            ) {
+                // COMENTARIO.
+                Text("0\nRecetas", textAlign = TextAlign.Center)
+
+                // COMENTARIO.
+                Text("0\nSeguidores", textAlign = TextAlign.Center)
+
+                // COMENTARIO.
+                Text("0\nSeguidos", textAlign = TextAlign.Center)
+            }
+            Spacer(modifier = Modifier.height(20.dp))
+
+            Button( // TODO: Funcionalidad del botón.
+                onClick = { appViewModel.restartBoolean() },
+                modifier = Modifier.size(150.dp, 50.dp)
+            ) {
+                Text("Seguir")
+            }
+        }
+    }
 }
